41feda14ac86786c1833208edaa4594589201dc5
peterywakim <57347565+peterywakim@users.noreply.github.com>
2021-04-15 14:10:16 +0200

    testing

---
+++ src/main/java/Page.java
@@ -0,0 +1,22 @@
+import java.io.Serializable;
+import java.util.Vector;
+
+/**
+ * 
+ */
+
+/**
+ * @author peter
+ *
+ */
+public class Page extends Vector<Object> implements Serializable {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
