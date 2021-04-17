d47ffdf91ff8390b6c2911b62380bc2f30b2fcac
ZiadAbdelmoniem <62437350+ZiadAbdelmoniem@users.noreply.github.com>
2021-04-17 13:16:04 +0200

    init

--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -1,6 +1,44 @@
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Vector;
 
-public class Table extends Vector<Page> implements Serializable {
+public class Table  implements Serializable {
+    ArrayList<Page> pages= new ArrayList<Page>();
+
+    public Table(){
+
+
+    }
+    public void serialT(){
+        try
+        {
+            //Saving of object in a file
+            FileOutputStream file = new FileOutputStream(filename);
+            ObjectOutputStream out = new ObjectOutputStream(file);
+
+            // Method for serialization of object
+            out.writeObject(object);
+
+            out.close();
+            file.close();
+
+            System.out.println("Object has been serialized");
+
+        }
+
+        catch(IOException ex)
+        {
+            System.out.println("IOException is caught");
+        }
+
+    }
+    public void unSerialT(){
+
+    }
+    public static void main(string[]args){
+
+    }
+
+
 
 }
